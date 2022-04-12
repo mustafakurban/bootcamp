b120a82a77c1d038e90bfc9d82b8a6f9612e073e
mustafa <mustafakurban155@gmail.com>
2022-04-12 13:44:00 +0300

    add sub,pub,custom msg

---
+++ scripts/custom_msg_pub.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import rospy
+from bootcamp.msg import example
+
+def talker():
+    pub = rospy.Publisher('custom_chatter', example, queue_size=10)
+    rospy.init_node('custom_talker', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        ex_pbj = example()
+        ex_pbj.first_name = "mustafa"
+        ex_pbj.last_name  = "kurban"
+
+        rospy.loginfo(ex_pbj)
+        pub.publish(ex_pbj)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
